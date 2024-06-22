29403414a26b31b7393ec56ff0ddd108505844a9
Lorenzo Marzocchetti <61746163+lmarzocchetti@users.noreply.github.com>
2024-06-22 12:48:14 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define i_TYPE Floats, float
+#include "stc/vec.h"
+
+#include "mimalloc.h"
+#include "sds.h"
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
