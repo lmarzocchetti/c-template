153e67f7a00719e94737ef5140bd3e28d58aa0b5
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2024-08-23 21:57:19 +0200

    Added Glib

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
-#include <raylib.h>
 
 int main (void) {
-    constexpr auto screen_width = 800;
-    constexpr auto screen_height = 600;
-
-    InitWindow(screen_width, screen_height, "Hello raylib master and C23");
-    SetTargetFPS(60);
-
-    while(!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawRectangle(100, 100, 100, 200, BLACK);
-        EndDrawing();
-    }
-
-    CloseWindow();
+    printf("Hello, World!");
     return 0;
 }
