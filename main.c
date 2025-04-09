a7fd328a416432f9567b4f434ad2798e6cb7c348
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2025-04-09 12:08:48 +0200

    Adjusted the c23 flag

--- main.c
+++ main.c
@@ -5,20 +5,24 @@
 #include <sds.h>
 
 int main(void) {
-    Floats nums = {0};
-    Floats_push(&nums, 30.f);
-    Floats_push(&nums, 10.f);
-    Floats_push(&nums, 20.f);
+	Floats nums = {0};
+	Floats_push(&nums, 30.f);
+	Floats_push(&nums, 10.f);
+	Floats_push(&nums, 20.f);
 
-    for (int i = 0; i < Floats_size(&nums); ++i)
-        printf(" %g", nums.data[i]);
+	for (int i = 0; i < Floats_size(&nums); ++i)
+		printf(" %g", nums.data[i]);
 
-    for (c_each(i, Floats, nums)) // Alternative and recommended way to iterate.
-        printf(" %g", *i.ref);      // i.ref is a pointer to the current element.
+	for (c_each(i, Floats, nums)) // Alternative and recommended way to iterate.
+		printf(" %g", *i.ref);      // i.ref is a pointer to the current element.
+	Floats_drop(&nums); // cleanup memory
 
-    Floats_drop(&nums); // cleanup memory
+	sds mystring = sdsnew("Hello World!");
+	printf("%s\n", mystring);
+	sdsfree(mystring);
 
-    sds mystring = sdsnew("Hello World!");
-    printf("%s\n", mystring);
-    sdsfree(mystring);
+	constexpr int a = 10;
+	printf("DEBUG %d\n", a);
+
+	return 0;
 }
